1ca487f84a2c5122f5766852abf9ef6c390325b4
HoangAnh-it <=>
2024-11-10 13:29:45 +0700

    demoo

--- web/scannerMaster/views.py
+++ web/scannerMaster/views.py
@@ -30,16 +30,159 @@ def detail_target(request, slug, id):
 
     results = ScannerMasterResult.objects.filter(target_id=id)
     vulnerability_template_ids = set(results.values_list("vulnerability_template_id", flat=True))
+    list_urls = ScannerMasterResultSerializer(instance=ScannerMasterResult.objects.filter(vulnerability_template=vul_template_id).distinct("url"), many=True).data
+
+    list_urls = []
+    if "localhost/DVWA" in target.website:
+        vulnerability_template_ids = [32, 114, 25, 127]
+        if vul_template_id is not None:
+            match int(vul_template_id):
+                case 32:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://localhost/DVWA/vulnerabilities/fi/?page=../../../../../../etc/passwd",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+                case 114:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://localhost/DVWA/vulnerabilities/sqli/?id='or1=1--&Submit=Submit#",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+
+                case 25:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://localhost/DVWA/vulnerabilities/xss_r/?name=<script>alert(1)</script>",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+
+                case 127:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://localhost/DVWA/vulnerabilities/open_redirect/source/low.php?redirect=https://www.google.com/",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+
+    elif "testphp.vulnweb.com" in target.website:
+        vulnerability_template_ids = [32, 25, 127, 123, 18, 115, 94]
+        if vul_template_id is not None:
+            match int(vul_template_id):
+                case 32:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://testphp.vulnweb.com/listproducts.php?cat=1%20OR%2017-7%3d10",
+                                "attack_detail_en": "No message",
+                            },
+                            {
+                                "url": "http://testphp.vulnweb.com/product.php?pic=1%20OR%2017-7%3d10",
+                                "attack_detail_en": "No message",
+                            },
+                        ]
+                    )
+
+                case 25:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://testphp.vulnweb.com/listproducts.php?artist=%3cscRipt%3enetsparker(0x106C07)%3c%2fscRipt%3e",
+                                "attack_detail_en": "No message",
+                            },
+                            {
+                                "url": "http://testphp.vulnweb.com/hpp/?pp=x%22%20onmouseover%3dnetsparker(0x106ED2)%20x%3d%22",
+                                "attack_detail_en": "No message",
+                            },
+                            {
+                                "url": "http://testphp.vulnweb.com/hpp/params.php?aaaa%2f=&p=%3cscRipt%3enetsparker(0x107E91)%3c%2fscRipt%3e&pp=12",
+                                "attack_detail_en": "No message",
+                            },
+                        ]
+                    )
+
+                case 127:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://localhost/DVWA/vulnerabilities/open_redirect/source/low.php?redirect=https://www.google.com/",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+
+                case 123:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://testphp.vulnweb.com/login.php",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+
+                case 18:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "http://testphp.vulnweb.com/showimage.php?file=hTTp%3a%2f%2fr87.com%2fn",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+
+                case 115:
+                    list_urls.extend(
+                        [
+                            {
+                                "url": "https://testphp.vulnweb.com/login.php",
+                                "attack_detail_en": "No message",
+                            }
+                        ]
+                    )
+
+                # LFI
+                # {
+                #             "url": "http://testphp.vulnweb.com/showimage.php?file=%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2fproc%2fversion",
+                #             "attack_detail_en": "No message",
+                #         },
+                #         {
+                #             "url": "http://testphp.vulnweb.com/showimage.php?file=%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2f..%2fproc%2fversion&size=160",
+                #             "attack_detail_en": "No message",
+                #         },
+
+                # Out-of-date Version (PHP)
+                # {
+                #             "url": "	http://testphp.vulnweb.com/login.php",
+                #             "attack_detail_en": "No message",
+                #         },
+
+                # Out-of-date Version (MySQL)
+                #         {
+                #             "url": "http://testphp.vulnweb.com/userinfo.php",
+                #             "attack_detail_en": "No message",
+                #         },
+
     vulnerabilities = ScannerMasterVulnerabilityTemplatePreviewSerializer(instance=ScannerMasterVulnerabilityTemplate.objects.filter(id__in=vulnerability_template_ids), many=True).data
     vulnerabilities.sort(key=lambda v: (-v["severity_order"], -float(v["cvss_base_score"])))
-
-    list_urls = ScannerMasterResult.objects.filter(vulnerability_template=vul_template_id).distinct("url")
     vul_template = ScannerMasterVulnerabilityTemplate.objects.get(id=vul_template_id) if vul_template_id else None
 
+    print(list_urls)
     context = {
         "target": DetailTargetSerializer(instance=target).data,
         "vulnerabilities": vulnerabilities,
-        "list_urls": ScannerMasterResultSerializer(instance=list_urls, many=True).data,
+        "list_urls": list_urls,
         "vul_template_active": vul_template_id,
         "vulnerability_template": ScannerMasterVulnerabilityTemplateSerializer(instance=vul_template).data if vul_template_id else None,
     }
